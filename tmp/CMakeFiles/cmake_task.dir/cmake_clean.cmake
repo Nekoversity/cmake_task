4b3ba88b3f58e83a0ee3b6d426e8e6a37281d6dd
nekovalue <nekovalue@gmail.com>
2022-01-17 20:34:37 +0200

    [U] First successful make

---
+++ tmp/CMakeFiles/cmake_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_task.dir/main.cpp.o"
+  "cmake_task.pdb"
+  "cmake_task"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
